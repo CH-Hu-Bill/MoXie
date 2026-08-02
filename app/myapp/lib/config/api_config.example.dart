9acb3adc4af3cd13eb56e294e12b960b3d567b9c
CH-Hu-Bill <CH-Hu-Bill@users.noreply.github.com>
2026-08-02 11:43:59 +0800

    Update api_config.example.dart to use 127.0.0.1

--- app/myapp/lib/config/api_config.example.dart
+++ app/myapp/lib/config/api_config.example.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://localhost:8000/web';
+  static const String baseUrl = 'http://127.0.0.1:8000/web';
   static const String apiEndpoint = '$baseUrl/app_api.php';
   static const String appVersion = '1.0.0';
 }
